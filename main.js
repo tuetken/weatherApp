2b4bf43c223fd1337b4ad3284188533c709e9b17
tyler <t.tuetken@outlook.com>
2025-03-24 14:46:27 -0400

    Added getWeather function and invald entry catch

---
+++ main.js
@@ -0,0 +1,12 @@
+function getWeather() {
+    const apiKey = '9239eb24e55b1799337022483ed0efcc';
+    const city = document.getElementById('city').value;
+
+    if (!city) {
+        alert('Please Enter a City');
+        return;
+    }
+
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+}
